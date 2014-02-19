b310be7235714ad18f7da62c215894f4e2615277
Dallan Quass <dallan@quass.org>
2014-02-19 16:26:20 -0600

    remove special characters from image filenames so we dont error when creating thumbnails

--- includes/SpecialUpload.php
+++ includes/SpecialUpload.php
@@ -271,13 +271,28 @@ class UploadForm {
 		return $result;
 	}
 
+
+	// WERELATE added
+	private function cleanFilename($filename) {
+      $unwanted_array = array(
+       'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+       'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+       'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+       'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+       'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y',
+       'Ğ'=>'G', 'İ'=>'I', 'Ş'=>'S', 'ğ'=>'g', 'ı'=>'i', 'ş'=>'s', 'ü'=>'u',
+       'ă'=>'a', 'Ă'=>'A', 'ș'=>'s', 'Ș'=>'S', 'ț'=>'t', 'Ț'=>'T');
+      return iconv('UTF-8', 'US-ASCII//TRANSLIT//IGNORE', strtr($filename, $unwanted_array ));
+	}
+
    /**
 	 * Constructor : initialise object
 	 * Get data POSTed through the form and assign them to the object
 	 * @param $request Data posted.
 	 */
 	function UploadForm( &$request ) {
-		$this->mDestFile          = $request->getText( 'wpDestFile' );
+// WERELATE - clean filename so we don't error when creating thumbnails
+		$this->mDestFile          = cleanFilename($request->getText( 'wpDestFile' ));
 // WERELATE - add target and id and people and families
 		$this->mTarget				  = $request->getText( 'target');
 		$this->mId					  = $request->getText( 'id');
