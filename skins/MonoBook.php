2f5ef0d354261005970a99f97bb951e3ed49bfe8
Dallan Quass <dallan@quass.org>
2016-09-12 09:10:53 -0600

    remove webengage

--- skins/MonoBook.php
+++ skins/MonoBook.php
@@ -552,10 +552,6 @@ END;
       <?php $this->html('reporttime') ?>
    </div>
 </div>
-<script id="_webengage_script_tag" type="text/javascript">
-var webengage; !function(e,t,n){function o(e,t){e[t[t.length-1]]=function(){r.__queue.push([t.join("."),arguments])}}var i,s,r=e[n],g=" ",l="init options track screen onReady".split(g),a="feedback survey notification".split(g),c="options render clear abort".split(g),p="Open Close Submit Complete View Click".split(g),u="identify login logout setAttribute".split(g);if(!r||!r.__v){for(e[n]=r={__queue:[],__v:"6.0",user:{}},i=0;i<l.length;i++)o(r,[l[i]]);for(i=0;i<a.length;i++){for(r[a[i]]={},s=0;s<c.length;s++)o(r[a[i]],[a[i],c[s]]);for(s=0;s<p.length;s++)o(r[a[i]],[a[i],"on"+p[s]])}for(i=0;i<u.length;i++)o(r.user,["user",u[i]]);setTimeout(function(){var f=t.createElement("script"),d=t.getElementById("_webengage_script_tag");f.type="text/javascript",f.async=!0,f.src=("https:"==t.location.protocol?"https://ssl.widgets.webengage.com":"http://cdn.widgets.webengage.com")+"/js/webengage-min-v-6.0.js",d.parentNode.insertBefore(f,d)})}}(window,document,"webengage");
-webengage.init("aa1323db");
-</script>
 <script type="<?php $this->text('jsmimetype') ?>">
 var pageRevid=<?php echo ($wgArticle != null ? $wgArticle->getRevIdFetched() : 0) ?>;
 var treeNames="<?php echo htmlspecialchars(implode('|', $this->data['trees'])); ?>";
