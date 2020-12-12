d61637ddbd4982bf631944fce3f053db9fb99bda
JanetBjorndahl <57600810+JanetBjorndahl@users.noreply.github.com>
2020-12-12 13:03:39 -0600

    Update SpecialGedcoms.php to prioritize link to new GEDCOM reviewer
    Flipped the links for the new GEDCOM reviewer and the old GEDCOM reviewer

--- extensions/gedcom/SpecialGedcoms.php
+++ extensions/gedcom/SpecialGedcoms.php
@@ -90,8 +90,8 @@ class GedcomsPage extends QueryPage {
 		$ulink = $skin->userLink( $userid, $result->ft_user ) . $skin->userToolLinks( $userid, $result->ft_user );
 		if (($result->fg_status >= FG_STATUS_READY && $result->fg_status <= FG_STATUS_ADMIN_REVIEW) ||
           $result->fg_status == FG_STATUS_HOLD) {
-			$filename = '<a href="http://www.werelate.org/gedcom/index.php?gedcomId='.$result->fg_id.'" rel="nofollow">'.htmlspecialchars($result->fg_gedcom_filename).'</a>';
-			$filename .= ' (<a href="https://www.werelate.org/gedcom-review/?gedcomId='.$result->fg_id.'" rel="nofollow">beta</a>)';
+			$filename = '<a href="https://www.werelate.org/gedcom-review/?gedcomId='.$result->fg_id.'" rel="nofollow">'.htmlspecialchars($result->fg_gedcom_filename).'</a>';
+			$filename .= ' (or <a href="http://www.werelate.org/gedcom/index.php?gedcomId='.$result->fg_id.'" rel="nofollow">Flash</a>)';
 		}
 		else {
 			$filename = htmlspecialchars($result->fg_gedcom_filename);
