272f2bbc844996b1fa50039efb63cad06bca1ae8
JanetBjorndahl <57600810+JanetBjorndahl@users.noreply.github.com>
2022-07-17 13:41:08 -0600

    SpecialDataQuality - tweaks to capturing comments
    Capture user comments when verifying an anomaly.
    When capturing comments, allow user to cancel.

--- report.js
+++ report.js
@@ -10,25 +10,33 @@ function toggleEnabled(checkId, checkValue, toggleId) {
 
 /* Add a template to a Talk page to indicate that an anomaly has been verified. The Talk page will be created if it doesn't already exist. */
 function addVerifiedTemplate(rowNum, pageId, ns, title, template, desc) {
-	$.getJSON('/w/index.php?action=ajax&rs=wfAddVerifiedTemplate&pid=' + pageId + '&ns=' + ns + '&title=' + title + '&template=' + template + '&desc=' + desc + '&callback=?', function(success) {
-     if (success) {
-       $('#' + 'verify' + rowNum).replaceWith('<span class="attn">&nbsp;Marked verified</span>');
-     }
-     else {
-       $('#' + 'verify' + rowNum).replaceWith('<span class="attn">&nbsp;Failed</span>');
-     }
-  })
+  var comments = encodeURIComponent(prompt("Enter any comments you wish to leave on the Talk page", "none")); 
+  // If user selected "cancel" (comments = 'null'), don't proceed with the template
+  if ( comments !== 'null' ) {
+	  $.getJSON('/w/index.php?action=ajax&rs=wfAddVerifiedTemplate&pid=' + pageId + '&ns=' + ns + '&title=' + title + '&template=' + template + '&desc=' + desc +
+          '&comments=' + comments + '&callback=?', function(success) {
+      if (success) {
+        $('#' + 'verify' + rowNum).replaceWith('<span class="attn">&nbsp;Marked verified</span>');
+      }  
+      else {
+        $('#' + 'verify' + rowNum).replaceWith('<span class="attn">&nbsp;Failed</span>');
+      }
+    })
+  }  
 }
 
-/* Add a template to a Talk page to indicate that the user doesn't want to see the Person/Family page on the DQ report for now. The Talk page will be created if it doesn't already exist. */
+/* Add a template to a Talk page to indicate that the user has defered resolution of issues on a Person/Family page for now. The Talk page will be created if it doesn't already exist. */
 function addDeferredTemplate(rowNum, pageId, ns, title) {
-  var comments = encodeURIComponent(prompt("Do you wish to leave comments on the Talk page?", "none")); 
-	$.getJSON('/w/index.php?action=ajax&rs=wfAddDeferredTemplate&pid=' + pageId + '&ns=' + ns + '&title=' + title + '&comments=' + comments + '&callback=?', function(success) {
-     if (success) {
-       $('#' + 'defer' + rowNum).replaceWith('<span class="attn">&nbsp;Deferred</span>');
-     }
-     else {
-       $('#' + 'defer' + rowNum).replaceWith('<span class="attn">&nbsp;Failed</span>');
-     }
-  })
+  var comments = encodeURIComponent(prompt("Enter any comments you wish to leave on the Talk page", "none")); 
+  // If user selected "cancel" (comments = 'null'), don't proceed with the template
+  if ( comments !== 'null' ) {
+	  $.getJSON('/w/index.php?action=ajax&rs=wfAddDeferredTemplate&pid=' + pageId + '&ns=' + ns + '&title=' + title + '&comments=' + comments + '&callback=?', function(success) {
+      if (success) {
+        $('#' + 'defer' + rowNum).replaceWith('<span class="attn">&nbsp;Deferred</span>');
+      }
+      else {
+        $('#' + 'defer' + rowNum).replaceWith('<span class="attn">&nbsp;Failed</span>');
+      }
+    })   
+  }
 }
